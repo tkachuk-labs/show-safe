94c3bbdb3c5544f2a4faae3fbab99e64caf5e4d4
Ilja Tkachuk <tkachuk.labs@gmail.com>
2020-05-30 17:24:56 +0300

    init nix-lib stack template

---
+++ nix/export-test-envs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#
+# app
+#
+
+export SHOW_SAFE_ENV="dev";
+export SHOW_SAFE_LOG_FORMAT="Bracket"; # Bracket | JSON
+export SHOW_SAFE_LIBPQ_CONN_STR="postgresql://nixbld1@localhost/show-safe";
+export SHOW_SAFE_ENDPOINT_PORT="4000";
---
+++ nix/release.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+./nix/bootstrap.sh
+
+NIXPKGS_ALLOW_BROKEN=1 nix-build ./nix/default.nix \
+  -I ssh-config-file=/tmp/.ssh/config \
+  --argstr hexOrganization tkachuk-labs \
+  --argstr hexApiKey $HEX_API_KEY \
+  --argstr robotSshKey $ROBOT_SSH_KEY \
+  --option sandbox false \
+  -v --show-trace
